74e4d2bc60772685a4dbfd95b10fe663dd8900d7
yukiagatsuma <yukiagatsuma@gmail.com>
2018-06-06 20:11:51 +0900

    1st commit

---
+++ app/views/artworks/_artwork.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! artwork, :id, :created_at, :updated_at
+json.url artwork_url(artwork, format: :json)
---
+++ test/system/artworks_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class ArtworksTest < ApplicationSystemTestCase
+  setup do
+    @artwork = artworks(:one)
+  end
+
+  test "visiting the index" do
+    visit artworks_url
+    assert_selector "h1", text: "Artworks"
+  end
+
+  test "creating a Artwork" do
+    visit artworks_url
+    click_on "New Artwork"
+
+    click_on "Create Artwork"
+
+    assert_text "Artwork was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Artwork" do
+    visit artworks_url
+    click_on "Edit", match: :first
+
+    click_on "Update Artwork"
+
+    assert_text "Artwork was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Artwork" do
+    visit artworks_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Artwork was successfully destroyed"
+  end
+end
