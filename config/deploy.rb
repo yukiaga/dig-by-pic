9971971f71a49af91416e688547593d11c0b9f5f
yukiagatsuma <yukiagatsuma@gmail.com>
2018-06-07 20:51:20 +0900

    add auto deploy function

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid only for current version of Capistrano
+lock "~> 3.10.2"
+
+set :application, 'dig-by-pic'
+set :repo_url,  'git@github.com:yukiaga/dig-by-pic.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['/Users/yukiagatsuma/important/AWS_key_pair/yukiagatsuma.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '54.64.230.63', user: 'ec2-user', roles: %w{app db web}
