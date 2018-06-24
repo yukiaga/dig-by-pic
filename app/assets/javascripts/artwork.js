425666c289f765184a798026951ab059468e6bb2
yukiagatsuma <yukiagatsuma@gmail.com>
2018-06-24 21:39:36 +0900

    update artwork.js

--- app/assets/javascripts/artwork.js
+++ app/assets/javascripts/artwork.js
@@ -12,40 +12,6 @@ $(document).on('turbolinks:load', function(){
     }
   });
 
-// infinitescrollのニューバージョンの場合
-  // var msnry = $main.data('masonry');
-
-  // $main.infiniteScroll( {
-  //   path : ".next a",
-  //   append : ".popup-modal",
-  //   outlayer : msnry,
-  //   scrollThreshold: 40,
-  //   history : false
-  // });
-
-// infinitescrollの旧バージョンの場合(今のjsのファイルはこっち)
-  // $main.imagesLoaded(function(){
-  //   $main.masonry({
-  //     itemSelector: '.box',
-  //     });
-  // });
-
-  // $main.infinitescroll({
-  //   navSelector: "nav.pagination",
-  //   nextSelector: "nav.pagination a[rel=next]",
-  //   itemSelector: ".box",
-  //   loading: {
-  //       finishedMsg: ' ',
-  //       img: '',
-  //   }
-  // },
-  // function( newElements ){
-  //   var $newElem = $(newElements);
-  //   $newElem.imagesLoaded(function(){
-  //     $main.masonry('appended',$newElem,true);
-  //   });
-  // });
-
   $('.popup-modal').magnificPopup({
     type: 'inline',
     preloader: true
