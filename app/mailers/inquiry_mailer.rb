9cd5576b5283fbc96fd10330d68d5d8f24274082
yukiagatsuma <yukiagatsuma@gmail.com>
2018-06-26 18:57:23 +0900

    update inquiry_mailer.rb

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -7,7 +7,7 @@ class InquiryMailer < ApplicationMailer
   def received_email(inquiry)
     @inquiry = inquiry
 
-    mail to: Rails.application.credentials.gmail_address
+    mail to: 'onsencouncil@gmail.com'
 
     mail subject: 'お問い合わせ'
 
